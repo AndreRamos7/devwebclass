19435dc5893db8dd1515b3a9014e24d017ab6b69
AndreRamos7 <andre-rammos@hotmail.com>
2026-03-19 09:29:12 -0300

    Projeto atualizado da Aula 18/03

---
+++ aula_pratica/script.js
@@ -0,0 +1,22 @@
+/* CRIA O ARRAY URI DE IMAGENS */
+imagens = [
+    "img/stranger-things-actors/Winona_Ryder.jpg" ,
+    "img/stranger-things-actors/Finn_Wolfhard.jpg",
+    "img/jogadores/neymar.jpg",
+    "img/jogadores/messi.jpeg",
+    "img/jogadores/ronaldo.jpeg",
+    "img/jogadores/zidane.jpeg"
+]
+
+/* CRIA A FUNÇÃO PARA TROCAR A IMAGEM */
+function trocarImagem(){
+    const img = document.getElementById("personagem");
+    // GERA UM NÚMERO ALEATÓRIO ENTRE 0 E O TAMANHO DO ARRAY DE IMAGENS
+    const indice = Math.floor(Math.random() * imagens.length);    
+    // TROCA A IMAGEM DO ELEMENTO COM O ID "personagem" PARA A IMAGEM CORRESPONDENTE AO NÚMERO ALEATÓRIO GERADO
+    img.src = imagens[indice];
+}
+// SELECIONA O ELEMENTO COM O ID "avancar" E ARMAZENA EM UMA VARIÁVEL
+const botaoAvancar = document.getElementById("avancar");
+/* CRIA O EVENTO PARA O BOTAO */
+botaoAvancar.addEventListener("click", trocarImagem);
